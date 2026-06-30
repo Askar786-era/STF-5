359fdc5b783d8ef286f86cd4e655b550ab458f13
Askar786-era <askar786era@gmail.com>
2026-06-30 21:36:40 +0530

    fix: correct mongoose pre-save hook signature for async function

--- models/Donor.js
+++ models/Donor.js
@@ -19,15 +19,10 @@ const donorSchema = new mongoose.Schema({
 donorSchema.index({ bloodGroup: 1, city: 1, state: 1 });
 
 // Hash password before saving
-donorSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (err) {
-        next(err);
-    }
+donorSchema.pre('save', async function() {
+    if (!this.isModified('password')) return;
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model('Donor', donorSchema);
